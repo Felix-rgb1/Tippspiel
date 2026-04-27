97ae91f11095406ae52306cc63ff109418c37fe8
Felix-1893 <felix.durst@fleigro.onmicrosoft.com>
2026-04-27 14:52:23 +0200

    reload fix

--- Tippspiel/frontend/src/App.jsx
+++ Tippspiel/frontend/src/App.jsx
@@ -96,6 +96,7 @@ function App() {
                 <Admin />
               </AdminRoute>
             } />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
       </BrowserRouter>
